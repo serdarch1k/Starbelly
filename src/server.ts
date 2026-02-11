ecca2b01cd33307c49572df0600909657f611e36
serdarch1k <sardormadgoziyev@gmail.com>
2026-02-11 17:04:13 +0900

    feat: create project standards

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ import mongoose from "mongoose";
 import app from "./app";
 
 mongoose
-   .connect(process.env.MONGO_URL as string, {})
-   .then((data) => {
-      console.log("MongoDB connection succeed");
-      const PORT = process.env.PORT ?? 3003;
-      app.listen(PORT, function () {
-         console.log(`The server is running successfully on port: ${PORT}`);
-      });
-   })
-   .catch((err) => console.log("ERROR on connection MongoDB", err));
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succeed");
+    const PORT = process.env.PORT ?? 3003;
+    app.listen(PORT, function () {
+      console.log(`The server is running successfully on port: ${PORT} \n`);
+    });
+  })
+  .catch((err) => console.log("ERROR on connection MongoDB", err));
