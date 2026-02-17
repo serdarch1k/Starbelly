a4e42b08fb23c540b8d370ac49225e4b558981c9
serdarch1k <sardormadgoziyev@gmail.com>
2026-02-17 15:58:53 +0900

    feat: develop admin frontend pages and integrate header & footer

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -11,7 +11,7 @@ const adminController: T = {};
 adminController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home Page");
+    res.render("home");
   } catch (err) {
     console.log("Error, goHome:", err);
   }
@@ -21,7 +21,7 @@ adminController.goHome = (req: Request, res: Response) => {
 adminController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("Error, getSignup:", err);
   }
@@ -31,7 +31,7 @@ adminController.getSignup = (req: Request, res: Response) => {
 adminController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("Error, getLogin:", err);
   }
