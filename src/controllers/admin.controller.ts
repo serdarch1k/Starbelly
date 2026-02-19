cc6883a8000fd6bbba8650ab30565f7827400ef7
serdarch1k <sardormadgoziyev@gmail.com>
2026-02-19 17:09:21 +0900

    fix: create sessions on login and signup process

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { LoginInput, MemberInput } from "../libs/types/member";
+import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
 
 const memberService = new MemberService();
@@ -38,16 +38,19 @@ adminController.getLogin = (req: Request, res: Response) => {
 };
 
 // process Signup
-adminController.processSignup = async (req: Request, res: Response) => {
+adminController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processSignup");
 
     const newMember: MemberInput = req.body;
     newMember.memberType = MemberType.ADMIN;
     const result = await memberService.processSignup(newMember);
-    // TODO: SESSIONS AUTHENTICATION
 
-    res.send(result);
+    // SESSIONS AUTHENTICATION
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error, processSignup:", err);
     res.send(err);
@@ -55,15 +58,18 @@ adminController.processSignup = async (req: Request, res: Response) => {
 };
 
 // process Login
-adminController.processLogin = async (req: Request, res: Response) => {
+adminController.processLogin = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processLogin");
 
     const input: LoginInput = req.body;
     const result = await memberService.processLogin(input);
-    // TODO: SESSIONS AUTHENTICATION
 
-    res.send(result);
+    // SESSIONS AUTHENTICATION
+    req.session.member = result;
+    req.session.save(function () {
+      res.send(result);
+    });
   } catch (err) {
     console.log("Error, processLogin:", err);
     res.send(err);
