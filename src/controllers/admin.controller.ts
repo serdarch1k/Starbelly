7e026dea93ea915ddb015e73633a4e306c0dab33
serdarch1k <sardormadgoziyev@gmail.com>
2026-03-15 22:43:57 +0900

    fix: modify admin controller processSignup and processLogin

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -3,7 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
-import Errors, { Message } from "../libs/Errors";
+import Errors, { HttpCode, Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -45,15 +45,19 @@ adminController.getLogin = (req: Request, res: Response) => {
 adminController.processSignup = async (req: AdminRequest, res: Response) => {
   try {
     console.log("processSignup");
+    const file = req.file;
+    if (!file)
+      throw new Errors(HttpCode.BAD_REQUEST, Message.SOMETHING_WENT_WRONG);
 
     const newMember: MemberInput = req.body;
+    newMember.memberImage = file?.path;
     newMember.memberType = MemberType.ADMIN;
     const result = await memberService.processSignup(newMember);
 
     // SESSIONS AUTHENTICATION
     req.session.member = result;
     req.session.save(function () {
-      res.send(result);
+      res.redirect("/admin/product/all");
     });
   } catch (err) {
     console.log("Error, processSignup:", err);
@@ -76,7 +80,7 @@ adminController.processLogin = async (req: AdminRequest, res: Response) => {
     // SESSIONS AUTHENTICATION
     req.session.member = result;
     req.session.save(function () {
-      res.send(result);
+      res.redirect("/admin/product/all");
     });
   } catch (err) {
     console.log("Error, processLogin:", err);
