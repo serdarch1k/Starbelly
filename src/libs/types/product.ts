003cb2783dfe0a077f3f1df5b7713ed682bd77ea
serdarch1k <sardormadgoziyev@gmail.com>
2026-03-16 11:58:55 +0900

    fix: modify updateChosenProduct business logic

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -31,3 +31,17 @@ export interface ProductInput {
   productImage?: string[];
   productViews?: number;
 }
+
+export interface ProductUpdateInput {
+  _id: ObjectId;
+  productStatus?: ProductStatus;
+  productCollection?: ProductCollection;
+  productName?: string;
+  productPrice?: number;
+  productLeftCount?: number;
+  productSize?: ProductSize;
+  productVolume?: number;
+  productDesc?: string;
+  productImage?: string[];
+  productViews?: number;
+}
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -33,7 +33,7 @@ routerAdmin.post(
   productController.createNewProduct,
 );
 routerAdmin.post(
-  "/product/:id",
+  "/product/:id", // params
   adminController.verifyAdmin,
   productController.updateChosenProduct,
 );
