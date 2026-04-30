186b8a16172a8f0658ca166af2552a35c388be06
serdarch1k <sardormadgoziyev@gmail.com>
2026-05-01 00:04:12 +0900

    fix: admin project refactoring

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -8,7 +8,6 @@ $(function () {
   fileTarget.on("change", function () {
     if (window.FileReader) {
       const uploadFile = $(this)[0].files[0];
-      console.log("uploadFile:", uploadFile);
       const fileType = uploadFile["type"];
       const validImageType = ["image/jpg", "image/jpeg", "image/png"];
       if (!validImageType.includes(fileType)) {
@@ -29,10 +28,10 @@ $(function () {
 
 // Frontend Validation
 function validateSignupForm() {
-  const memberNick = $(".member-nick").val();
-  const memberPhone = $(".member-phone").val();
-  const memberPassword = $(".member-password").val();
-  const confirmPassword = $(".confirm-password").val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
   if (
     memberNick === "" ||
--- src/public/js/users.js
+++ src/public/js/users.js
@@ -3,10 +3,7 @@ console.log("Users frontend javascript file");
 $(function () {
   $(".member-status").on("change", function (e) {
     const id = e.target.id;
-    console.log("id:", id);
-
     const memberStatus = $(`#${id}.member-status`).val();
-    console.log("memberStatus:", memberStatus);
 
     //Axios updateChosenUser (Rest Api) ga murojat
     axios
@@ -17,7 +14,6 @@ $(function () {
       .then((response) => {
         console.log("response:", response);
         const result = response.data;
-        console.log("result:", result);
 
         if (result.data) {
           console.log("User updated!");
